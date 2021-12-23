1d209025c757e024b49963ccdc77cc32ff05fe90
junxiang <junxiang.oriint@gmail.com>
2021-12-23 14:59:07 +0800

    [update] v-for :key with index

--- pages/medicine.vue
+++ pages/medicine.vue
@@ -33,9 +33,9 @@
       </div>
       <div class="W100">
         <div
+          v-for="(cat, index) in formData.cats"
           class="d_flex record_item"
-          :key="cat.notice"
-          v-for="cat in formData.cats"
+          :key="`${cat.notice}${index}`"
         >
           <div class="name">{{ cat.name }}</div>
           <div class="detail">
